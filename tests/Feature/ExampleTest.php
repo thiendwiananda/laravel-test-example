1e7737e0321cbbcfc9d763bc3d261cddc72fe657
ciky <rizky.ananda@sehatq.com>
2021-10-18 16:23:37 +0700

    code improvement

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,14 +16,15 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+    
     public function test_hard_dependency()
     {
-        Mockery::mock('overload:' . Elastic::class)->shouldReceive('fetchFromElastic')->once()->andReturn('ini akal akalan ciky');
+        Mockery::mock('overload:' . Elastic::class)->shouldReceive('fetchFromElastic')->andReturn('mutated');
 
         $response = $this->get('/hard');
 
         $response->assertJson([
-            "message" => "this hard dependency"
+            "message" => "mutated"
         ]);
     }
     
@@ -32,14 +33,14 @@ class ExampleTest extends TestCase
         $this->instance(
             Elastic::class,
             Mockery::mock(Elastic::class, function (MockInterface $mock) {
-                $mock->shouldReceive('fetchFromElastic')->once()->andReturn("this hard dependency");
+                $mock->shouldReceive('fetchFromElastic')->andReturn("mutated");
             })
         );
 
         $response = $this->get('/injection');
 
         $response->assertJson([
-            "message" => "ini akal akalan ciky"
+            "message" => "mutated"
         ]);
     }
 }
